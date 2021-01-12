8f2cc9845179d6c9f62d910b5ee217654fd40b3f
agillies8 <andrew.gillies@gmail.com>
2021-01-11 17:59:30 -0800

    using accelStepper lib

--- arduino/ROSArmWithHoming/ROSArmWithHoming.ino
+++ arduino/ROSArmWithHoming/ROSArmWithHoming.ino
@@ -1,6 +1,6 @@
 #include <ros.h> //http://wiki.ros.org/rosserial_arduino/Tutorials/Arduino%20IDE%20Setup
-#include <FlexyStepper.h> //https://github.com/Stan-Reifel/SpeedyStepper/blob/master/Documentation.pdf
 #include <geometry_msgs/Pose.h>
+#include <AccelStepper.h>
 
 ros::NodeHandle node_handle;
 
@@ -8,19 +8,20 @@ geometry_msgs::Pose move_axis_relative;
 geometry_msgs::Pose joints;
 geometry_msgs::Pose bump_axis;
 
+ros::Publisher arduino_joint_publisher("arduino_joint_publisher", &joints);
 
 // Connections to driver
-const int dirPin1 = 5;  // Direction for axis 1
-const int stepPin1 = 2;// Step for axis 1
-const int dirPin2 = 6;  // Direction for axis 2
-const int stepPin2 = 3; // Step for axis 2
-const int dirPin3 = 7;  // Direction for axis 3
-const int stepPin3 = 4; // Step for axis 3
+#define dirPin1  5  // Direction for axis 1
+#define stepPin1  2// Step for axis 1
+#define dirPin2  6  // Direction for axis 2
+#define stepPin2  3 // Step for axis 2
+#define dirPin3  7  // Direction for axis 3
+#define stepPin3  4 // Step for axis 3
 
-const int LED_PIN = 13;
-const int LIMIT_SWITCH_PIN1 = 9;
-const int LIMIT_SWITCH_PIN2 = 10;
-const int LIMIT_SWITCH_PIN3 = 11;
+#define LED_PIN  13
+#define LIMIT_SWITCH_PIN1  9
+#define LIMIT_SWITCH_PIN2  10
+#define LIMIT_SWITCH_PIN3  11
 
 int axis_1_cmd = 0;
 int axis_2_cmd = 0;
@@ -35,31 +36,19 @@ bool begin_homing = false;
 
 bool stopFlag = false;
 
-FlexyStepper axis1;
-FlexyStepper axis2;
-FlexyStepper axis3;
+AccelStepper axis1(1, stepPin1, dirPin1);
+AccelStepper axis2(1, stepPin2, dirPin2);
+AccelStepper axis3(1, stepPin3, dirPin3);
 
-
-ros::Publisher arduino_joint_publisher("arduino_joint_publisher", &joints);
 
 void move_axis_callback(const geometry_msgs::Pose& move_axis_relative) {
   axis_1_cmd = move_axis_relative.position.x;
   axis_2_cmd = move_axis_relative.position.y;
   axis_3_cmd = move_axis_relative.position.z;
 
-  joints.position.x=axis_1_cmd;
-  joints.position.y=axis_2_cmd;
-  joints.position.z=axis_3_cmd;
-  arduino_joint_publisher.publish( &joints );
-
-  delay(2000);
-  
-  joints.position.x=axis1.getCurrentPositionInSteps();
-  joints.position.y=axis2.getCurrentPositionInSteps();
-  joints.position.z=axis3.getCurrentPositionInSteps();
-  arduino_joint_publisher.publish( &joints );
-
-  delay(2000);
+  axis1.move(axis_1_cmd);
+  axis2.move(axis_2_cmd);
+  axis3.move(axis_3_cmd);
   begin_relative_move = true;
 
 
@@ -79,15 +68,18 @@ void bump_axis_callback(const geometry_msgs::Pose& bump_axis) {
   axis_3_dir = int(bump_axis.position.z);
   begin_homing = bool(bump_axis.orientation.w);
 
-//  if( (axis_1_dir==0) && (axis_2_dir==0) && (axis_3_dir==0)){
-//    stopFlag=true;
-//    begin_bump = false;
-//  }
+  axis1.move(axis_1_dir*1000);
+  axis2.move(axis_2_dir*1000);
+  axis3.move(axis_3_dir*1000);
+
+  if( (axis_1_dir==0) && (axis_2_dir==0) && (axis_3_dir==0)){
+    stopFlag=true;
+    begin_bump = false;
+  }
   
   begin_bump = true;
 
 }
-
 
 ros::Subscriber<geometry_msgs::Pose> arduino_sub1("move_axis_relative", &move_axis_callback);
 ros::Subscriber<geometry_msgs::Pose> arduino_sub2("bump_axis", &bump_axis_callback);
@@ -99,161 +91,113 @@ void setup() {
   node_handle.subscribe(arduino_sub1);
   node_handle.subscribe(arduino_sub2);
 
-
-  // Setup the steppers with speedy stepper lib
-  axis1.connectToPins(stepPin1, dirPin1);
-  axis2.connectToPins(stepPin2, dirPin2);
-  axis3.connectToPins(stepPin3, dirPin3);
-
   pinMode(LED_PIN, OUTPUT);
   pinMode(LIMIT_SWITCH_PIN1, INPUT_PULLUP);
   pinMode(LIMIT_SWITCH_PIN2, INPUT_PULLUP);
   pinMode(LIMIT_SWITCH_PIN3, INPUT_PULLUP);
 
   // set the speed and acceleration rates for the stepper motor
-  axis1.setStepsPerRevolution(3200);
-  axis1.setSpeedInRevolutionsPerSecond(2.0);
-  axis1.setAccelerationInRevolutionsPerSecondPerSecond(1.0);
+  axis1.setMaxSpeed(500.0);
+  axis1.setAcceleration(500.0);
   
-  axis2.setStepsPerRevolution(3200);
-  axis2.setSpeedInRevolutionsPerSecond(2.0);
-  axis2.setAccelerationInRevolutionsPerSecondPerSecond(1.0);
+  axis2.setMaxSpeed(500.0);
+  axis2.setAcceleration(500.0);
 
-  axis3.setStepsPerRevolution(3200);
-  axis3.setSpeedInRevolutionsPerSecond(2.0);
-  axis3.setAccelerationInRevolutionsPerSecondPerSecond(1.0);
+  axis3.setMaxSpeed(500.0);
+  axis3.setAcceleration(500.0);
 
 }
 void loop() {
 
-  joints.position.x=axis1.getCurrentPositionInSteps();
-  joints.position.y=axis2.getCurrentPositionInSteps();
-  joints.position.z=axis3.getCurrentPositionInSteps();
-  arduino_joint_publisher.publish( &joints );
-
   if (begin_relative_move==true){
       begin_relative_move = false;
-      axis1.setTargetPositionRelativeInSteps(axis_1_cmd);
-      axis2.setTargetPositionRelativeInSteps(axis_2_cmd);
-      axis3.setTargetPositionRelativeInSteps(axis_3_cmd);
-    
-      while(!axis1.motionComplete() || !axis2.motionComplete() || !axis3.motionComplete() ){
-            axis1.processMovement();
-            axis2.processMovement();
-            axis3.processMovement();
 
-            joints.position.x=axis1.getCurrentPositionInSteps();
-            joints.position.y=axis2.getCurrentPositionInSteps();
-            joints.position.z=axis3.getCurrentPositionInSteps();
-            arduino_joint_publisher.publish( &joints );
+      while(axis1.isRunning() || axis2.isRunning() || axis3.isRunning() ){
+
+            axis1.run();
+            axis2.run();
+            axis3.run();
+      
             node_handle.spinOnce();
             
             if (stopFlag==true) //add stop command, axis limits, timeout here
             {
-              axis1.setTargetPositionToStop();
-              axis2.setTargetPositionToStop();
-              axis3.setTargetPositionToStop();
+              axis1.stop();
+              axis2.stop();
+              axis3.stop();
               stopFlag = false;
               begin_relative_move = false;
               }       
       }
-
-   if ( axis1.motionComplete() && axis2.motionComplete() && axis3.motionComplete() ){
-      begin_relative_move = false;
-      joints.position.x=55.55;
-      joints.position.y=55.55;
-      joints.position.z=55.55;
-      arduino_joint_publisher.publish( &joints );
-
-      delay(5000);
-    }
-
  }
 
-  if (begin_bump){
+   if (begin_bump==true){
       begin_bump = false;
-      axis1.setTargetPositionRelativeInSteps(axis_1_dir*500);
-      axis2.setTargetPositionRelativeInSteps(axis_2_dir*500);
-      axis3.setTargetPositionRelativeInSteps(axis_3_dir*500);
 
-      joints.position.x=axis_1_dir;
-      joints.position.y=axis_2_dir;
-      joints.position.z=axis_3_dir;
-      arduino_joint_publisher.publish( &joints );
-      delay(3000);
-      
-      while(!axis1.motionComplete() || !axis2.motionComplete() || !axis3.motionComplete() )
-      {
-            axis1.processMovement();
-            axis2.processMovement();
-            axis3.processMovement();
-  
-            joints.position.x=axis1.getCurrentPositionInSteps();
-            joints.position.y=axis2.getCurrentPositionInSteps();
-            joints.position.z=axis3.getCurrentPositionInSteps();
-            arduino_joint_publisher.publish( &joints );
+      while(axis1.isRunning() || axis2.isRunning() || axis3.isRunning() ){
+
+            axis1.run();
+            axis2.run();
+            axis3.run();
+            
             node_handle.spinOnce();
-
-            //will change move command if subscriber updates dir values
-            axis1.setTargetPositionRelativeInRevolutions(axis_1_dir*500);
-            axis2.setTargetPositionRelativeInRevolutions(axis_2_dir*500);
-            axis3.setTargetPositionRelativeInRevolutions(axis_3_dir*500);
-
+            
             if (stopFlag==true) //add stop command, axis limits, timeout here
             {
-            axis1.setTargetPositionToStop();
-            axis2.setTargetPositionToStop();
-            axis3.setTargetPositionToStop();
-            axis1.processMovement();
-            axis2.processMovement();
-            axis3.processMovement();
-            stopFlag=false;
-            }
+              axis1.stop();
+              axis2.stop();
+              axis3.stop();
+              stopFlag = false;
+              begin_bump = false;
+              }       
       }
-    }
+ }
+
+ 
 
     if (begin_homing){
+        begin_homing = false;
 
-        const float homingSpeed = 0.25;
-        const float maxHomingRotation = 10;
-        const int directionTowardHome1 = -1;// direction to move toward limit switch: 1 goes positive direction, -1 backward
-        const int directionTowardHome2 = -1;
-        const int directionTowardHome3 = -1;  
+          while (digitalRead(LIMIT_SWITCH_PIN1)) {  // Make the Stepper move CCW until the switch is activated   
+              axis1.moveTo(100000);  // Set the position to move to
+              axis1.run();  // Start moving the stepper
+              delay(5);
+                node_handle.spinOnce();
+          }
+          axis1.setCurrentPosition(0);
+          axis1.runToNewPosition(-3600);
+          axis1.setCurrentPosition(0);
 
-        if(axis1.moveToHomeInRevolutions(directionTowardHome1, homingSpeed, maxHomingRotation, LIMIT_SWITCH_PIN1) != true)
-        {
-          while(true)
-          {
-            digitalWrite(LED_PIN, HIGH);
-            delay(50);
-            digitalWrite(LED_PIN, LOW);
-            delay(50);
+          
+          while (digitalRead(LIMIT_SWITCH_PIN2)) {  // Make the Stepper move CCW until the switch is activated   
+              axis2.moveTo(-100000);  // Set the position to move to
+              axis2.run();  // Start moving the stepper
+              delay(5);
+                node_handle.spinOnce();
           }
-        }
+          axis2.setCurrentPosition(0);
+          axis2.runToNewPosition(2300);
+          axis2.setCurrentPosition(0);
 
-        if(axis2.moveToHomeInRevolutions(directionTowardHome2, homingSpeed, maxHomingRotation, LIMIT_SWITCH_PIN2) != true)
-        {
-          while(true)
-          {
-            digitalWrite(LED_PIN, HIGH);
-            delay(50);
-            digitalWrite(LED_PIN, LOW);
-            delay(50);
+
+          while (digitalRead(LIMIT_SWITCH_PIN3)) {  // Make the Stepper move CCW until the switch is activated   
+              axis3.moveTo(100000);  // Set the position to move to
+              axis3.run();  // Start moving the stepper
+              delay(5);
+                node_handle.spinOnce();
           }
-        }
-        if(axis3.moveToHomeInRevolutions(directionTowardHome3, homingSpeed, maxHomingRotation, LIMIT_SWITCH_PIN3) != true)
-        {
-          while(true)
-          {
-            digitalWrite(LED_PIN, HIGH);
-            delay(50);
-            digitalWrite(LED_PIN, LOW);
-            delay(50);
-          }
-        }
-        begin_homing= false;
-      
+          axis3.setCurrentPosition(0);  
+          axis3.runToNewPosition(-800);
+          axis2.setCurrentPosition(0);
+
+          
+
     }
+  joints.position.x=axis1.currentPosition();
+  joints.position.y=axis2.currentPosition();
+  joints.position.z=axis3.currentPosition();
+  arduino_joint_publisher.publish( &joints );
+
   node_handle.spinOnce();
   delay(1);
 }
